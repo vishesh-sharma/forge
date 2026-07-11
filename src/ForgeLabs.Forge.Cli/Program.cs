ec29df0504abbe4fd02280d0c2883afd6b509bb5
Vishesh Sharma <sharmavishesh@outlook.com>
2026-07-11 18:53:51 +0000

    feat: bootstrap Forge CLI

--- src/ForgeLabs.Forge.Cli/Program.cs
+++ src/ForgeLabs.Forge.Cli/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("========================================");
+Console.WriteLine("           Forge v0.1");
+Console.WriteLine("    Engineering Toolkit for ForgeLabs");
+Console.WriteLine("========================================");
+Console.WriteLine();
+Console.WriteLine("Initializing...");
